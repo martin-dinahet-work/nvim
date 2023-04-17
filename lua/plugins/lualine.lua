d00020f6d0504cbd009cd48a6d7a8218964dbc58
Martin Dinahet <martin.dinahet.work@gmail.com>
2023-04-18 00:22:00 +0200

    Made lualine look better

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -21,9 +21,9 @@ require('lualine').setup {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = {'filetype'},
     lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_z = {}
   },
   inactive_sections = {
     lualine_a = {},
